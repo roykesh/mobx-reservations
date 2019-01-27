ac08c740a478be99b0f23a414b1762f4c937d72e
roykesh <42666643+roysh13@users.noreply.github.com>
2019-01-27 19:30:48 +0200

    exercises in progress

--- src/index.js
+++ src/index.js
@@ -7,13 +7,14 @@ import { Provider } from 'mobx-react'
 import GeneralStore from './stores/GeneralStore'
 import RestaurantStore from './stores/RestStore'
 
-const stores = {GeneralStore, 
-                RestaurantStore}
-ReactDOM.render(<Provider {... stores}>
-                    <App />
-                </Provider>, document.getElementById('root'));
+const stores = {
+    GeneralStore,
+    RestaurantStore
+}
+ReactDOM.render(
+    <Provider {...stores}>
+        <App />
+    </Provider>, document.getElementById('root')
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
--- src/stores/RestStore.js
+++ src/stores/RestStore.js
@@ -1,42 +1,48 @@
-import { observable, computed, action } from  'mobx'
+import { observable, computed, action } from 'mobx'
 
 class Res {
-    @observable name
-    @observable id = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(2, 10) 
-    //giving a random short string ID
-    @observable numPeople
-    @observable completed = false
-    constructor(name, numPeople) {
-        this.name = name
-        this.numPeople = numPeople
-    }
+  @observable name
+  @observable id = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(2, 10)
+  @observable numPeople
+  @observable completed = false
+  constructor(name, numPeople) {
+    this.name = name
+    this.numPeople = numPeople
+  }
 }
 
 class RestaurantStore {
-    @observable reservations = []
-    @observable numTables = 10
-    @computed get totalReservations() {
-        return this.reservations.length
-    } 
-    @computed get openTables() { //automatically caluclates the number of tables avalible, only when the state is affected
-        let counter = 0
-        this.reservations.forEach(r => !r.completed ? counter ++: null)
-        return (this.numTables - counter)
-    }
-    @computed get restPopulation() {
-        // calculate the number of people in the restaurant now
-    }
-    @computed get completedTables() {
-        //calculate the number of tables that have been completed
-    }
-    @action addRes = (name, numPeople) => {
-        this.reservations.push(new Res(name, numPeople))
-    }
-    @action completeRes = (id) => {
-        //find the reservation and mark it as completed
-        //after you write this function, add some conditional rendering on compelted tables
-        //e.g. strike through our a different color - this will happen on your react, not here.
-    }
+  @observable reservations = []
+  @observable numTables = 10
+  @computed get totalReservations() {
+    return this.reservations.length
+  }
+  @computed get openTables() { //automatically caluclates the number of tables avalible, only when the state is affected
+    let counter = 0
+    this.reservations.forEach(r => !r.completed ? counter++ : null)
+    return (this.numTables - counter)
+  }
+  @computed get restPopulation() {
+    let peopleCounter = 0
+    this.reservations.forEach(r => !r.completed ? peopleCounter += r.numPeople : null)
+    return peopleCounter
+  }
+  @computed get completedTables() {
+    //calculate the number of tables that have been completed
+    let completeTables = 0
+    this.reservations.forEach(r => r.completed ? completeTables++ : null)
+    return completeTables
+  }
+  @action addRes = (name, numPeople) => {
+    this.reservations.push(new Res(name, numPeople))
+  }
+  @action completeRes = (id) => {
+    //find the reservation and mark it as completed
+    //after you write this function, add some conditional rendering on completed tables
+    //e.g. strike through our a different color - this will happen on your react, not here.
+    let reservation = this.reservations.find(r => r.id === id)
+    reservation.completed = true
+  }
 }
 const HaAchim = new RestaurantStore()
 HaAchim.addRes("Bernard", 4)
